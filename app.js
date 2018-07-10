e34c0bf1157a6f2007f65820af0dd00b4f2c8986
Alinnimp <linnimonteirop@gmail.com>
2018-07-10 12:42:07 -0300

    versao 0.0.6

--- app.js
+++ app.js
@@ -1,67 +1,53 @@
-var btn = document.getElementById("button");
-btn.disabled = false;
-btn.addEventListener("click", tweet);
-// console.log("text");
-areaText.addEventListener('input-area', expandBox);
-function tweet() {
-    var tweetBox = document.getElementById("input-area");
-    var newTweet = document.createElement("p");
-    newTweet.textContent = tweetBox.value;
-    
-    var inputText = document.getElementById("input-text");
-    inputText.appendChild(newTweet);
+var btn = document.getElementById('button');
+var textoArea = document.getElementById('input-area');
+var charCont = document.getElementById('cont');
+charCont.textContent = 140;
+btn.disabled = true;
+
+function colorsCaracteres() {
+    charCont.textContent = (140 - textoArea.value.length);
+    console.log(textoArea.value.length);
+    if (textoArea.value.length === 0 || textoArea.value === ' ') {
+        btn.disabled = true;
+    } else if (textoArea.value.length < 20) {
+        charCont.classList.add('color-def');
+        charCont.classList.add('colorUm');
+        btn.disabled = true;
+    } else if (textoArea.value.length >= 110 && textoArea.value.length < 120) {
+        charCont.classList.add('color-def');
+        charCont.classList.add('colorDois');
+    } else if (textoArea.value.length >= 120 && textoArea.value.length <= 140) {
+        charCont.classList.add('color-def');
+        charCont.classList.add('colorTres');
+        btn.disabled = false;
+    } else {
+        charCont.classList = 'color-def';
+        btn.disabled = false;
+    }
+}
+
+function boxTextSize() {
+    textoArea = event.currentTarget;
+    textoArea.style.height = 'auto';
+    textoArea.style.height = textoArea.scrollHeight + 'px';
+}
+
+
+function userPostText() {
     event.preventDefault();
-    tweetBox.value = "";
-    
-// console.log("newTweet");
- } 
- function limiteText(value) {        
-    var quant = 140;
-    var total = value.length;
-    if(total <= quant) {
-        rest = quant - total;
-        document.getElementById('cont').innerHTML = rest; 
-    } 
-    if (areaText.value === 0 || areaText.value === '') {
-        btn.disabled = true;
-    } else {
-        btn.disabled = false;
-    }
+    if (textoArea.value) {
+        var postTextOutput = document.getElementById('input-text');
+        var newUserPost = document.createElement('li');
+        newUserPost.textContent = textoArea.value;
+        var dateOfPost = document.createElement('span');
+        var currentTime = new Date();
+        dateOfPost.textContent = 'Hora: ' + currentTime.getHours() + '\:' + currentTime.getMinutes();
+        newUserPost.appendChild(dateOfPost);
+        postTextOutput.appendChild(newUserPost);
+        console.log(userText.length);
+    }    
 }
 
-function caracter (){
-    cont.textContent = (140 - input-text.value.length);
-    console.log(input-text.value.length);
-    if (input-text.value.length === 0 || input-text.value === ' ') {
-        btn.disabled = true;
-    } else if (input-text.value.length > 140) {
-        cont.classList = 'colorDef';
-        cont.classList.add('colorUm');
-        btn.disabled = true;
-    } else if (input-text.value.length >= 110 && input-text.value.length < 120) {
-        cont.classList = 'color-def';
-        cont.classList.add('colorDois');
-    } else if (input-text.value.length >= 120 && input-text.value.length <= 140) {
-        cont.classList = 'color-def';
-        cont.classList.add('colorTres');
-        btn.disabled = false;
-    } else {
-        cont.classList = 'color-def';
-        btn.disabled = false;
-    }
-}
-
-// var date = new Date();
-//        var content = document.createTextNode(areaText + " " + date.getHours()+":"+date.getMinutes());//adicionando hora//
-//        var main = document.getElementById("boxOne")
-//          container.appendChild(twitt);
-//          twitt.appendChild(content);
-//          twitt.classList.add ("twitt");
-//          main.appendChild(twitt);
-//          words.value = ' ';
-
-function expandBox() {
-    if (areaText.scrollHeight > areaText.offsetHeight) {
-        areaText.rows += 1;
-    }
-}
+textoArea.addEventListener('keydown', boxTextSize);
+textoArea.addEventListener('keyup', colorsCaracteres);
+btn.addEventListener("click", userPostText);

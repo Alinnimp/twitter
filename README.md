e947c76451d7e7ebfa0d8a820e9168928c153c53
Alinnimp <linnimonteirop@gmail.com>
2018-10-29 16:19:53 -0300

    alt Readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# twitter
+# Twitter
+
+Desafio do Sprint 2 do bootcamp Laboratoria
+O desafio era criar uma versão para desktop simplificada do Twitter, contendo uma área para postar, um contador de caracteres, uma timeline para ver as mensagens postadas, as quais deveriam ter data e hora.
--- app.js
+++ app.js
@@ -1,51 +1,50 @@
 var btn = document.getElementById('button');
 var textoArea = document.getElementById('input-area');
 var charCont = document.getElementById('cont');
+
 charCont.textContent = 140;
 btn.disabled = true;
 
-function colorsCaracteres() {
-    charCont.textContent = (140 - textoArea.value.length);
-    console.log(textoArea.value.length);
-    if (textoArea.value.length === 0 || textoArea.value === ' ') {
-        btn.disabled = true;
-    } else if (textoArea.value.length < 20) {
-        charCont.classList.add('color-def');
-        charCont.classList.add('colorUm');
-        btn.disabled = true;
-    } else if (textoArea.value.length >= 110 && textoArea.value.length < 120) {
-        charCont.classList.add('color-def');
-        charCont.classList.add('colorDois');
-    } else if (textoArea.value.length >= 120 && textoArea.value.length <= 140) {
-        charCont.classList.add('color-def');
-        charCont.classList.add('colorTres');
-        btn.disabled = false;
-    } else {
-        charCont.classList = 'color-def';
-        btn.disabled = false;
-    }
-}
-
 function boxTextSize() {
-    textoArea = event.currentTarget;
-    textoArea.style.height = 'auto';
-    textoArea.style.height = textoArea.scrollHeight + 'px';
-}
+  textoArea = event.currentTarget;
+  textoArea.style.height = 'auto';
+  textoArea.style.height = textoArea.scrollHeight + 'px';
+};
 
+function colorsCaracteres() {
+  charCont.textContent = (140 - textoArea.value.length);
+  if (textoArea.value.length === 0 || textoArea.value === ' ') {
+    btn.disabled = true;
+  } else if (textoArea.value.length < 20) {
+    charCont.classList.add('color-def');
+    charCont.classList.add('colorUm');
+    btn.disabled = true;
+  } else if (textoArea.value.length >= 110 && textoArea.value.length < 120) {
+    charCont.classList.add('color-def');
+    charCont.classList.add('colorDois');
+  } else if (textoArea.value.length >= 120 && textoArea.value.length <= 140) {
+    charCont.classList.add('color-def');
+    charCont.classList.add('colorTres');
+    btn.disabled = false;
+  } else {
+    charCont.classList = 'color-def';
+    btn.disabled = false;
+  }
+};
 
 function userPostText() {
-    event.preventDefault();
-    if (textoArea.value) {
-        var postTextOutput = document.getElementById('input-text');
-        var newUserPost = document.createElement('li');
-        newUserPost.textContent = textoArea.value;
-        var dateOfPost = document.createElement('span');
-        var currentTime = new Date();
-        dateOfPost.textContent = 'Hora: ' + currentTime.getHours() + '\:' + currentTime.getMinutes();
-        newUserPost.appendChild(dateOfPost);
-        postTextOutput.appendChild(newUserPost);
-        console.log(userText.length);
-    }    
+  event.preventDefault();
+  if (textoArea.value) {
+    var postTextOutput = document.getElementById('output-text');
+    var newUserPost = document.createElement('li');
+    newUserPost.textContent = textoArea.value;
+    var dateOfPost = document.createElement('span');
+    var currentTime = new Date();
+    dateOfPost.textContent = 'Hora: ' + currentTime.getHours() + '\:' + currentTime.getMinutes();
+    newUserPost.appendChild(dateOfPost);
+    postTextOutput.appendChild(newUserPost);
+    console.log(userText.length);
+  }
 }
 
 textoArea.addEventListener('keydown', boxTextSize);
